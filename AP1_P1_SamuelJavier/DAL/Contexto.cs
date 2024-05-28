1cc80a97af80fddbd78847b5e426f4cb67a8a283
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-05-28 01:01:17 -0400

    Add project files.

---
+++ AP1_P1_SamuelJavier/DAL/Contexto.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace AP1_P1_SamuelJavier.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) 
+        : base(options) { }
+}
