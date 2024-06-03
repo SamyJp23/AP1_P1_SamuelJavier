92bdfe134af587a0f45395881c85c533da7e4b5e
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-06-03 19:10:02 -0400

    Agregando el key al IdArticulo

--- AP1_P1_SamuelJavier/Models/Articulos.cs
+++ AP1_P1_SamuelJavier/Models/Articulos.cs
@@ -6,7 +6,9 @@ namespace AP1_P1_SamuelJavier.Models;
 public class Articulos
     
 {
-    
+
+    [Key]
+    [Range(1, int.MaxValue,ErrorMessage = "El id debe ser mayor que 0")]
       public int IdArticulo { get; set; }
     [Required(ErrorMessage = "El campo es obligatorio.")]
       public string? Descripcion { get; set; }
