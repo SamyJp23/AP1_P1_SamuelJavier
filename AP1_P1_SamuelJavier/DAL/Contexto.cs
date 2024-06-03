9ad863ef85e5ef74fd1a811e90efa46cb7be2d36
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-06-03 18:37:02 -0400

    Creando el contexto

--- AP1_P1_SamuelJavier/DAL/Contexto.cs
+++ AP1_P1_SamuelJavier/DAL/Contexto.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using AP1_P1_SamuelJavier.Models;
 namespace AP1_P1_SamuelJavier.DAL;
 
 public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) 
         : base(options) { }
+
+    DbSet<Articulos> Articulos { get; set; }
 }
